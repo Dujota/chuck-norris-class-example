90447145be6d1870f87f38d448926e27fbe4c72e
Denis <ddujota@internationalsafety.com>
2021-05-01 16:10:07 -0500

    added request for input to chuck norris jokes

--- controllers/jokes.js
+++ controllers/jokes.js
@@ -1,4 +1,21 @@
+const request = require('request');
+
+const categoriesURL = 'https://api.chucknorris.io/jokes/categories';
+const jokeURL = 'https://api.chucknorris.io/jokes/random?category=';
+
 function index(req, res) {
-  res.render('index', { categories: ['music', 'dev'] });
+  request(categoriesURL, function (err, resp, categoriesJSON) {
+    const categories = JSON.parse(categoriesJSON);
+
+    if (req.query.category) {
+      request(jokeURL + req.query.category, function (err, resp, jokeJSON) {
+        const joke = JSON.parse(jokeJSON);
+        res.render('index', { categories, joke: joke.value, categoryChosen: req.query.category });
+      });
+    } else {
+      res.render('index', { categories, joke: null, categoryChosen: null });
+    }
+  });
 }
+
 module.exports = { index };
